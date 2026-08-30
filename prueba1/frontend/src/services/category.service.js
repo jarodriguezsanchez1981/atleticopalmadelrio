93d2307d6b74f8ee5778e0198f98b9207b61a0ca
Jose Antonio Rodriguez Sanchez <jarodriguezsanchez@gmail.com>
2026-08-30 12:29:04 +0200

    Delete prueba1 directory

--- prueba1/frontend/src/services/category.service.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export default {
-  list: () => api.get('/categories').then((r) => r.data),
-  create: (payload) => api.post('/categories', payload).then((r) => r.data),
-  update: (id, payload) => api.put(`/categories/${id}`, payload).then((r) => r.data),
-  remove: (id) => api.delete(`/categories/${id}`).then((r) => r.data),
-};
--- prueba1/frontend/src/services/player.service.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export default {
-  listByCategory: (categoryId) => api.get(`/players/category/${categoryId}`).then((r) => r.data),
-  getOne: (id) => api.get(`/players/${id}`).then((r) => r.data),
-  create: (payload) => api.post('/players', payload).then((r) => r.data),
-  update: (id, payload) => api.put(`/players/${id}`, payload).then((r) => r.data),
-  remove: (id) => api.delete(`/players/${id}`).then((r) => r.data),
-};
